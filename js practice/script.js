7b9c4449f19e38d1949591ee885988cf9f087b45
Krisztian Toth <korte@kortefoto.hu>
2020-05-01 11:04:48 +0200

    First try

---
+++ js practice/script.js
@@ -0,0 +1,16 @@
+function getGreetingTo (name) {
+    console.log('Hello ' + name)
+
+};
+
+getGreetingTo('James')
+
+
+function printValues(a) {
+    a.forEach(element => {
+        console.log(element)
+    });
+
+}
+
+printValues([2, 5, 8, 11, 15])
