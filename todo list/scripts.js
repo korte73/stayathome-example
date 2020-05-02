5af4c0f527dbd0661a1183a5429a317e80f85e54
Krisztian Toth <korte@kortefoto.hu>
2020-05-02 21:55:40 +0200

    stupid mistype

--- todo list/scripts.js
+++ todo list/scripts.js
@@ -1,7 +1,8 @@
 console.log("System Online");
 let newTodo = "x";
+console.log(newTodo)
 $("#ibutton").click( function() {
-    let newTodo = document.getElementById("ifield").nodeValue;
+    newTodo = document.getElementById("ifield").value;
     console.log(newTodo);
     $("#todolist").append("<li>"+newTodo+"</li>");
 
